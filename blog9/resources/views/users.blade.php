1e403273632d07ae288e81441e08f5a164423ebe
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-23 09:42:37 +0200

    Add basic styling to form

--- blog9/resources/views/users.blade.php
+++ blog9/resources/views/users.blade.php
@@ -2,8 +2,15 @@
 
 <form action="users" method="POST">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
+<br>
+    <br>
+
     <input type="text" name="username" placeholder="Username">
+<br>
+    <br>
     <input type="password" name="password" placeholder="Password">
+<br>
+    <br>
     <input type="submit" value="Login">
 
 </form>
